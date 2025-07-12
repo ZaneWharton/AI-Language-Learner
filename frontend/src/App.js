06869ed35cfa5fbb9609defea51eeb9f70cfc1b5
ZaneWharton <zanewharton@gmail.com>
2025-07-11 20:08:23 -0500

    Removes nested Git repo in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-4xl text-blue-500 font-bold">App Is Working</h1>
+    </div>
+  )
+}
